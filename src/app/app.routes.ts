29ebc7c67b6713adc81d06b1c81952683958ec71
chivinsaing24 <chivin.saing@akilabs.co>
2026-03-12 18:44:18 +0900

    header footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DrinksComponent } from './pages/drinks/drinks.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  { path: 'about', component: AboutComponent, title: 'About' },
+  { path: 'drinks', component: DrinksComponent, title: 'Drinks' },
+  { path: 'contact', component: ContactComponent, title: 'Contact' },
+  { path: '**', redirectTo: 'home' }
+];
