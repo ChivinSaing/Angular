98efddd7a596a5d0c99ba5a56d0a8f50ea836273
SaingChivin <saingchivin24@gmail.com>
2026-03-12 23:37:43 +0700

    card box

--- src/app/components/card-box/card-box.ts
+++ src/app/components/card-box/card-box.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-card-box',
   standalone: true,
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './card-box.html',
   styleUrl: './card-box.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class CardBox {}
