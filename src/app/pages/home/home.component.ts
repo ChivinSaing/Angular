1f9f9fe47bc7feede004dfa2945dcbdbd3ff2ed9
chivinsaing24 <chivin.saing@akilabs.co>
2026-03-23 16:21:34 +0900

    test

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -31,14 +31,14 @@ export class HomeComponent {
   readonly items: CardBoxItemWithId[] = [
     {
       id: 1,
-      name: 'Lavender Latte',
+      name: 'Espresso',
       price: 5.75,
       inventoryStatus: 'INSTOCK',
-      imageUrl: 'https://primefaces.org/cdn/primeng/images/demo/product/brown-purse.jpg',
+      imageUrl: 'https://ungroundedcoffee.com/image/cache/data/Products/Beverage/IcedAmericano-600x600-500x500.jpg',
     },
     {
       id: 2,
-      name: 'Matcha Mint Tea',
+      name: 'Milk Tea',
       price: 5.75,
       inventoryStatus: 'LOWSTOCK',
       imageUrl: 'https://material.angular.dev/assets/img/examples/shiba2.jpg',
@@ -56,6 +56,96 @@ export class HomeComponent {
       price: 72,
       inventoryStatus: 'OUTOFSTOCK',
       imageUrl: 'https://media.istockphoto.com/id/1325991061/photo/matcha-latte-green-milk-foam-cup-on-wood-table-at-cafe-trendy-powered-tea-trend-from-japan.jpg?s=612x612&w=0&k=20&c=a7cV9mdPwPj93BrxoFrJXEdA71RsOnXIOzVF90CYPsQ=',
+    },
+  ];
+  readonly hotDrinks: CardBoxItemWithId[] = [
+    {
+      id: 1,
+      name: 'Lavender Latte',
+      price: 5.75,
+      inventoryStatus: 'INSTOCK',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrjyHlkViXf1t9FlpAnmlozT9DZSvPbFi-Lg&s',
+    },
+    {
+      id: 2,
+      name: 'Americano',
+      price: 5.75,
+      inventoryStatus: 'LOWSTOCK',
+      imageUrl: '/images/americano.png',
+    },
+    {
+      id: 3,
+      name: 'Cappuccino',
+      price: 5.75,
+      inventoryStatus: 'INSTOCK',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAVt__A8bvTk9UbAp6N28awtRlQjJxssNuLA&s',
+    },
+    {
+      id: 4,
+      name: 'Caramel Macchiato',
+      price: 72,
+      inventoryStatus: 'OUTOFSTOCK',
+      imageUrl: 'https://thelittlestcrumb.com/wp-content/uploads/salted-caramel-macchiato-6.jpg',
+    },
+  ];
+  readonly iceDrinks: CardBoxItemWithId[] = [
+    {
+      id: 1,
+      name: 'Iced Americano',
+      price: 5.75,
+      inventoryStatus: 'INSTOCK',
+      imageUrl: 'https://ungroundedcoffee.com/image/cache/data/Products/Beverage/IcedAmericano-600x600-500x500.jpg',
+    },
+    {
+      id: 2,
+      name: 'Iced Latte',
+      price: 5.75,
+      inventoryStatus: 'LOWSTOCK',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSANcWK53dWB_kChQU9l_etT4616mOkJYD_2A&s',
+    },
+    {
+      id: 3,
+      name: 'Iced Cappuccino',
+      price: 5.75,
+      inventoryStatus: 'INSTOCK',
+      imageUrl: 'https://grindthosebeans.com/wp-content/uploads/2024/10/Iced-Cappuccino-photo.png',
+    },
+    {
+      id: 4,
+      name: 'Iced Caramel Macchiato',
+      price: 72,
+      inventoryStatus: 'OUTOFSTOCK',
+      imageUrl: 'https://thelittlestcrumb.com/wp-content/uploads/salted-caramel-macchiato-6.jpg',
+    },
+  ];
+  readonly tea: CardBoxItemWithId[] = [
+    {
+      id: 1,
+      name: 'Iced Green Tea',
+      price: 5.75,
+      inventoryStatus: 'INSTOCK',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKqzwxZXcDbHRjacc2QPG1FneKvGvdl9P0Fw&s',
+    },
+    {
+      id: 2,
+      name: 'Iced Lemon Tea',
+      price: 5.75,
+      inventoryStatus: 'LOWSTOCK',
+      imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/026/282/306/small/iced-lemon-tea-on-plastic-take-away-glass-isolated-on-white-background-with-copy-space-ai-generated-photo.jpg',
+    },
+    {
+      id: 3,
+      name: 'Iced Matcha Latte',
+      price: 5.75,
+      inventoryStatus: 'INSTOCK',
+      imageUrl: 'https://131462261.cdn6.editmysite.com/uploads/1/3/1/4/131462261/WSWIE42L25DRZK4UFOL7IMF6.jpeg',
+    },
+    {
+      id: 4,
+      name: 'Milk Tea',
+      price: 72,
+      inventoryStatus: 'OUTOFSTOCK',
+      imageUrl: 'https://ricelifefoodie.com/wp-content/uploads/2024/10/ice-cold-Hokkaido-Milk-Tea-with-boba-.jpg',
     },
   ];
 
